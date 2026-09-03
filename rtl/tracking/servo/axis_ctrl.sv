6958bf51ff52d41527bc2a6e76d18ee28d367ae7
mumallaeng <mumallaeng@outlook.com>
2026-09-03 12:55:06 +0900

    rename ctrl_mode_mux IO

--- rtl/tracking/servo/axis_ctrl.sv
+++ rtl/tracking/servo/axis_ctrl.sv
@@ -24,7 +24,7 @@ module axis_ctrl (
     input  logic [8:0] delta_x,
     input  logic [7:0] delta_y,
     input  logic       update,
-    output logic [7:0] angle_x,
-    output logic [7:0] angle_y
+    output logic [7:0] angle_pan,
+    output logic [7:0] angle_tilt
 );
 endmodule
--- rtl/tracking/servo/servo_top.sv
+++ rtl/tracking/servo/servo_top.sv
@@ -1,35 +1,15 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 2026/09/01 11:47:50
-// Design Name: 
-// Module Name: servo_top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
-module servo_top(
-    input [8:0] error_x,
-    input [7:0] error_y,
-    input vaild,
-    input done,
-    input mode,
-    input [8:0] joy_x,
-    input [7:0] joy_y,
-    input joy_done,
-    output pwm_pan,
-    output pwn_tilt
-    );
+module servo_top (
+    input  logic [8:0] frame_dx,
+    input  logic [7:0] frame_dy,
+    input  logic       frame_valid,
+    input  logic       frame_done,
+    input  logic       mode,
+    input  logic [8:0] joy_dx,
+    input  logic [7:0] joy_dy,
+    input  logic       joy_done,
+    output logic       pwm_pan,
+    output logic       pwm_tilt
+);
 endmodule
